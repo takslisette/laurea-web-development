c7c6bedda4da86e29300740109548abb5cc7257b
Lisette <lit00054@laurea.fi>
2026-09-10 12:05:50 +0300

    "ikärajan" lisääminen scriptiin js

--- WS02_JS_basics/js/script.js
+++ WS02_JS_basics/js/script.js
@@ -1,9 +1,20 @@
 console.log("Hello World!");
+
 let name = "Lisette";
 let age = 23;
 let favoriteAnimal = "dog";
+
 console.log(name);
 console.log(age);
 console.log(favoriteAnimal);
+
 let userName = prompt("What is your name?");
+
 alert("Hello " + userName + "!");
+
+let userAge = prompt("How old are you?");
+if (userAge >= 18) {
+    alert("You are an adult!");
+} else {
+    alert("You are a minor!");
+}

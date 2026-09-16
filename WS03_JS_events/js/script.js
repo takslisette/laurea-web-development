5fef832754d5bb21306d1aa2e6cfda878df94083
Lisette <lit00054@laurea.fi>
2026-09-16 13:11:44 +0300

    js lisäys

--- WS03_JS_events/js/script.js
+++ WS03_JS_events/js/script.js
@@ -2,4 +2,19 @@ function showTable() {
     const animal = "Tiikeri";
     const habitat = "Metsä";
     const diet = "Liha";
+
+    const table = `
+        <table>
+            <tr>
+                <th>Eläin</th>
+                <th>Elinympäristö</th>
+                <th>Ruokavalio</th>
+            </tr>
+            <tr>
+                <td>${animal}</td>
+                <td>${habitat}</td>
+                <td>${diet}</td>
+            </tr>
+        </table>
+    `;
 }
